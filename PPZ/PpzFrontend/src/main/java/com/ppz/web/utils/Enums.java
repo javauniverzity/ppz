c193f0162f103d0a5ef10e1d2f4ebe5683ea942b
Pavel Lukes <pavel.lukes@etnetera.cz>
2015-05-20 21:40:57 +0200

    neni componenta

--- PPZ/PpzFrontend/src/main/java/com/ppz/web/utils/Enums.java
+++ PPZ/PpzFrontend/src/main/java/com/ppz/web/utils/Enums.java
@@ -3,9 +3,7 @@ package com.ppz.web.utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
-import org.springframework.stereotype.Component;
 
-@Component
 public class Enums {
 	
 	@Autowired
